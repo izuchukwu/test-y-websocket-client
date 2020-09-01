47a4e08dd454e8ca50308f84cfa9c3f567ac177b
Izuchukwu <izuchukwu@hey.com>
2020-09-01 11:28:37 -0400

    Adds yjs and local persistence

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,10 +1,29 @@
-import React, {useLayoutEffect, useState, useRef, MutableRefObject} from 'react'
+import React, {useLayoutEffect, useEffect, useState, useRef, MutableRefObject} from 'react'
+import * as Y from 'yjs'
+import {IndexeddbPersistence} from 'y-indexeddb'
 import './Canvas.css'
 
+let doc = new Y.Doc()
+const persistence = new IndexeddbPersistence('test-y-websocket', doc)
+persistence.whenSynced.then(() => {
+    console.log('persistence: got doc')
+    const boxMap = doc.getMap('box')
+    if (!boxMap.has('origin')) {
+        boxMap.set('origin', {x: 0, y: 0})
+    }
+})
+
 function Canvas() {
+    const [docReady, setDocReady] = useState(doc.getMap('box').has('origin'))
+
+    useEffect(() => {
+        // Show Box on Doc Ready effect
+        persistence.whenSynced.then(() => setDocReady(true))
+    })
+
     return (
         <div className="Canvas">
-            <Box/>
+            {docReady && <Box/>}
         </div>
     )
 }
@@ -15,31 +34,45 @@ function Box() {
     const [holdPosition, setHoldPosition] = useState({x: 0, y: 0})
     const [origin, setOrigin] = useState({x: 0, y: 0})
 
-    const setBoxHeldToTrue = (event: MouseEvent) => {
-        setHoldPosition({
-            x: event.clientX - origin.x,
-            y: event.clientY - origin.y
+    useEffect(() => {
+        // Sync Origin With Doc effect
+        const docOrigin = doc.getMap('box').get('origin')
+        setOrigin(docOrigin)
+        boxRef.current.style.transform = `translate(${docOrigin.x}px, ${docOrigin.y}px)`
+
+        doc.getMap('box').observe(() => {
+            const docOrigin = doc.getMap('box').get('origin')
+            setOrigin(docOrigin)
+            boxRef.current.style.transform = `translate(${docOrigin.x}px, ${docOrigin.y}px)`
         })
-        setBoxHeld(true)
-    }
-    const setBoxHeldToFalse = () => setBoxHeld(false)
-
-    const dragBox = (event: MouseEvent) => {
-        if (boxHeld) {
-            event.stopImmediatePropagation()
-            event.preventDefault()
-
-            const newOrigin = {
-                x: event.clientX - holdPosition.x,
-                y: event.clientY - holdPosition.y
-            }
-            setOrigin(newOrigin)
-
-            boxRef.current.style.transform = `translate(${newOrigin.x}px, ${newOrigin.y}px)`
-        }
-    }
+    }, [boxRef])
 
     useLayoutEffect(() => {
+        const setBoxHeldToTrue = (event: MouseEvent) => {
+            setHoldPosition({
+                x: event.clientX - origin.x,
+                y: event.clientY - origin.y
+            })
+            setBoxHeld(true)
+        }
+        const setBoxHeldToFalse = () => setBoxHeld(false)
+
+        const dragBox = (event: MouseEvent) => {
+            if (boxHeld) {
+                event.stopImmediatePropagation()
+                event.preventDefault()
+
+                const newOrigin = {
+                    x: event.clientX - holdPosition.x,
+                    y: event.clientY - holdPosition.y
+                }
+                setOrigin(newOrigin)
+                doc.getMap('box').set('origin', newOrigin)
+
+                boxRef.current.style.transform = `translate(${newOrigin.x}px, ${newOrigin.y}px)`
+            }
+        }
+
         const box = boxRef.current
         box.addEventListener('mousedown', setBoxHeldToTrue)
         window.addEventListener('mouseup', setBoxHeldToFalse)
